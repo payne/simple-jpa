314db493ddc1f71900a44ec0cab86a4b8ee633cd
Matt Payne <Payne@MattPayne.org>
2019-06-21 15:27:46 -0500

    From start.spring.io

---
+++ src/test/java/org/mattpayne/fun/jpa/cucumber/simplejpa/SimpleJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mattpayne.fun.jpa.cucumber.simplejpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
